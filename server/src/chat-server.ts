d1f7c3e1d3c5ada68ccbe446dd18e77dccf645e4
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-19 16:09:30 -0500

    Change variables privates for #

--- server/src/chat-server.ts
+++ server/src/chat-server.ts
@@ -7,10 +7,10 @@ import { config } from '../config';
 
 export class ChatServer {
   public static readonly PORT: any = config.api.port;
-  private app!: express.Application;
-  private server!: Server;
-  private io!: socketIo.Server;
-  private port!: string | number;
+  #app!: express.Application;
+  #server!: Server;
+  #io!: socketIo.Server;
+  #port!: string | number;
 
   constructor() {
     this.createApp();
@@ -19,32 +19,32 @@ export class ChatServer {
     this.sockets();
     this.listen();
   }
-  private createApp(): void {
-    this.app = express();
+  protected createApp(): void {
+    this.#app = express();
   }
 
-  private createServer(): void {
-    this.server = createServer(this.app);
+  protected createServer(): void {
+    this.#server = createServer(this.#app);
   }
 
-  private config(): void {
-    this.port = ChatServer.PORT;
+  protected config(): void {
+    this.#port = ChatServer.PORT;
   }
 
-  private sockets(): void {
-    this.io = socketIo(this.server);
+  protected sockets(): void {
+    this.#io = socketIo(this.#server);
   }
 
-  private listen(): void {
-    this.server.listen(this.port, () => {
-      console.log('Running server on the port %s', this.port);
+  protected listen(): void {
+    this.#server.listen(this.#port, () => {
+      console.log('Running server on the port %s', this.#port);
     });
 
-    this.io.on('connect', (socket: any) => {
-      console.log('Connected client on port %s', this.port);
+    this.#io.on('connect', (socket: any) => {
+      console.log('Connected client on port %s', this.#port);
       socket.on('message', (msg: Message) => {
         console.log('[Server](message): %s', JSON.stringify(msg));
-        this.io.emit('message', msg);
+        this.#io.emit('message', msg);
       });
       socket.on('disconnect', () => {
         console.log('Client disconnected');
@@ -52,6 +52,6 @@ export class ChatServer {
     });
   }
   public getApp(): express.Application {
-    return this.app;
+    return this.#app;
   }
 }
