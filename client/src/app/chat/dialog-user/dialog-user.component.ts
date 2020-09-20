d10df7def084fd9673f11f91d316f27e5589f1e3
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-20 17:07:33 -0500

    added component by modal users

---
+++ client/src/app/chat/dialog-user/dialog-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tcc-dialog-user',
+  templateUrl: './dialog-user.component.html',
+  styleUrls: ['./dialog-user.component.scss']
+})
+export class DialogUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
